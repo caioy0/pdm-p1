56ae96b847fc0b103d39c3892db03152dbc1e644
caioy0 <caio.yugo@proton.me>
2026-04-12 15:30:12 -0300

    feat(p1): dependencies for react

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-// import CapturarDados from './components/CapturarDados.jsx'
+import 'primereact/resources/primereact.min.css'
+import 'primereact/resources/themes/nano/theme.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.min.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
